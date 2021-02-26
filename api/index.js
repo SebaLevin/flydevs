f596a4e0fb41eb17a47faacdd5ff312c9c1e7bc3
SebaLevin <sebalevin@hotmail.com.ar>
2021-02-26 12:13:08 -0300

    fixes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 const routes = require("./routes/routes.js");
 require("dotenv").config();
 
 const port = process.env.PORT || 8080;
+
+mongoose.connect(process.env.DATA_BASE, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    }  
+);
+
+mongoose.connection.once('open', () => {
+    console.log("MongoDB Connected!")
+});
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
